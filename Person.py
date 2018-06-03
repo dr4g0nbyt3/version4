8acdf5c9715538b7227b86bf1470de82359a7d47
dr4g0nbyt3 <dr4g0nbyt3@gmail.com>
2018-06-03 18:12:55 -0500

    Added Person Class.

---
+++ Person.py
@@ -0,0 +1,28 @@
+class Person:
+
+    # Person skill set
+
+    def __init__(self):
+        self.strength = 4
+        self.agility = 3
+        self.accuracy = 3
+        self.speed = 8
+        self.energy = 10
+        self.archery = 2
+        self.magic = 4
+        self.hunting = 4
+        self.gathering = 4
+        self.fishing = 4
+        self.woodcutting = 4
+        self.mining = 5
+        self.smithing = 5
+        self.fishing = 5
+        self.cooking = 4
+        self.farming = 4
+        self.prayer = 4
+        self.construction = 4
+        self.creativity = 3
+        self.alchemy = 4
+        self.crafting = 4
+        self.combat = 10
+        
--- Tribe.py
+++ Tribe.py
@@ -1,5 +1,7 @@
 class Tribe:
 
+    # skills: Social, Economic, Housing,
+
     def __init__(self):
         self.size = 2
         self.population = 5
@@ -7,6 +9,9 @@ class Tribe:
         self.type = 1
         self.skillPredilection = 3
         self.property[tiles] = 1
+
+        for p in range(self.population):
+            self.persons.append(Person())
 
     def grow(self):
         self.size += 1
