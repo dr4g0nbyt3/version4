dd82fd154ecd4d5a2b944eb5bb5f260c7df2cfab
dr4g0nbyt3 <dr4g0nbyt3@gmail.com>
2018-06-07 01:25:46 -0500

    Started on the plant and tree classes.

---
+++ Plant.py
@@ -0,0 +1,9 @@
+class Plant:
+
+    # I really want to have these be player and tribe harvestable.
+
+    def __init__(self, name, rarity):
+        self.name = name
+        self.rarity = rarity
+
+
---
+++ Tree.py
@@ -0,0 +1,5 @@
+class Tree:
+
+    def __init__(self, name, rarity):
+        self.name = name
+        self.rarity = rarity
